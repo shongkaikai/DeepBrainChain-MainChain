8f2bed939251854d9d4efe48be00b336907ce93f
bobo <dongkaige@gmail.com>
2021-07-05 16:03:19 +0800

    feat: use presets ERC20 template

--- dbc_erc20.sol
+++ dbc_erc20.sol
@@ -2,31 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+// import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract DBCToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Deepbrainchain Token", "DBC") {
-        _mint(msg.sender, initialSupply);
-        root = msg.sender;
-    }
+contract DBCERC20 is ERC20PresetMinterPauser {
+    bytes32 public constant BURN_ROLE = keccak256("BURN_ROLE");
 
-    address root;
-    function set_root(address new_root) public {
-        require(msg.sender == root, "DBCERC20: only root");
-        root = new_root;
+    constructor() ERC20PresetMinterPauser("Deepbrainchain Token", "DBC") {
+        _setupRole(BURN_ROLE, _msgSender());
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 15;
     }
 
-    function mint(address account, uint256 amount) public {
-        require(msg.sender == root, "DBCERC20: only root");
-        _mint(account, amount);
-    }
-
-    function burn(address account, uint256 amount) public {
-        require(msg.sender == root, "DBCERC20: only root");
+    function sudo_burn(address account, uint256 amount) public {
+        require(hasRole(BURN_ROLE, _msgSender()), "DBCERC20: must have burn role to burn");
         _burn(account, amount);
     }
 }
