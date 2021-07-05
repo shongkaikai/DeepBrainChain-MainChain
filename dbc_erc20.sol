f98fc1a1e3070ff9eb084ab5dae48d93214734a7
bobo <dongkaige@gmail.com>
2021-07-05 10:49:36 +0800

    fix: use root to operation

--- dbc_erc20.sol
+++ dbc_erc20.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/token/ERC20/ERC20.sol";
 
 contract DBCToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Deepbrainchain Token", "DBC") public {
+    constructor(uint256 initialSupply) ERC20("Deepbrainchain Token", "DBC") {
         _mint(msg.sender, initialSupply);
         root = msg.sender;
     }
@@ -21,12 +21,12 @@ contract DBCToken is ERC20 {
     }
 
     function mint(address account, uint256 amount) public {
-        require(account == root, "DBCERC20: only root");
+        require(msg.sender == root, "DBCERC20: only root");
         _mint(account, amount);
     }
 
     function burn(address account, uint256 amount) public {
-        require(account == root, "DBCERC20: only root");
+        require(msg.sender == root, "DBCERC20: only root");
         _burn(account, amount);
     }
 }
